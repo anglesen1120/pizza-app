1d4db841374be1c2073ba0ddb2dc981164cddea9
Michael Olusegun <mikkyboy2005@gmail.com>
2021-01-21 13:17:08 +0100

    bottom navigation and page view added

---
+++ lib/pages/main_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:pizza_app/pages/cart_screen.dart';
+import 'package:pizza_app/pages/home.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedItem = 0;
+  var _pageController = PageController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('Instant Pizza'),
+      //   backgroundColor: Colors.red,
+      // ),
+      body: PageView(
+        onPageChanged: (index) {
+          setState(() {
+            _selectedItem = index;
+            print(_selectedItem);
+          });
+        },
+        controller: _pageController,
+        children: [
+          Home(),
+          CartScreen(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedItem,
+        selectedItemColor: Colors.red,
+        onTap: (index) {
+          _selectedItem = index;
+          _pageController.animateToPage(
+            _selectedItem,
+            duration: Duration(microseconds: 200),
+            curve: Curves.linear,
+          );
+        },
+        items: [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Cart',
+            icon: Icon(Icons.shopping_cart_outlined),
+          ),
+        ],
+      ),
+    );
+  }
+}
